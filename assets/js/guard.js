e56baaf9cd4c673b06e581f6731981e4fdd94c36
SameerXD3459 <yadavsameer448@gmail.com>
2026-01-08 16:06:07 +0530

    commit2

--- assets/js/guard.js
+++ assets/js/guard.js
@@ -1,6 +1,2 @@
 document.addEventListener("contextmenu", e => e.preventDefault());
-document.onkeydown = function(e){
-    if(e.keyCode==123 || (e.ctrlKey && e.shiftKey) || (e.ctrlKey && e.keyCode==85)) {
-        return false;
-    }
-};
+document.onkeydown = e => !(e.keyCode==123 || (e.ctrlKey && e.shiftKey) || (e.ctrlKey && e.keyCode==85));
